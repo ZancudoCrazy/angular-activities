5e147f31aadbc38447c1ede38671c572fc0d77ef
zancudo <adriansa1997@gmail.com>
2025-12-26 20:20:56 -0700

    finish 02-bases section

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,17 +1,23 @@
-import { Injectable, signal } from "@angular/core";
+import { effect, Injectable, signal } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
+
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+}
 
 
 @Injectable({ providedIn: 'root'})
 export class DragonballService{
 
-    characters = signal<Character[]>([
-        {id: 1,name: 'Goku', power: 9001},
-        {id: 2,name: 'Vegeta', power: 9000}
-    ]);
+    characters = signal<Character[]>(loadFromLocalStorage());
 
     addNewCharacter(character: Character){
+      this.characters.update((list) => [...list, character]);
+    }
 
-    this.characters.update((list) => [...list, character]);
-  }
+    saveToLocalStorage = effect( () => {
+      
+      localStorage.setItem('characters', JSON.stringify(this.characters()));
+    })
 }
